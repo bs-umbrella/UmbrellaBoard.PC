104f79dd6707d00a15eb5264b3cc1608aaafb696
RedBrumbler <redbrumbler.official+github@gmail.com>
2024-04-04 01:23:23 +0200

    Register tags

--- UmbrellaBoard/Plugin.cs
+++ UmbrellaBoard/Plugin.cs
@@ -3,6 +3,8 @@ using IPALogger = IPA.Logging.Logger;
 using IPAConfig = IPA.Config.Config;
 using SiraUtil.Zenject;
 using UmbrellaBoard.Installers;
+using UmbrellaBoard.UI.Tags;
+
 using IPA.Config.Stores;
 using Zenject;
 
@@ -15,6 +17,15 @@ namespace UmbrellaBoard
         public Plugin(IPALogger logger, IPAConfig ipaConfig, Zenjector zenjector)
         {
             Config config = ipaConfig.Generated<Config>();
+
+            // register our tags
+            BeatSaberMarkupLanguage.BSMLParser.instance.RegisterTag(new CarouselTag());
+
+            BeatSaberMarkupLanguage.BSMLParser.instance.RegisterTag(new OpenPageButtonTag());
+            BeatSaberMarkupLanguage.BSMLParser.instance.RegisterTag(new OpenPageClickableImageTag());
+            BeatSaberMarkupLanguage.BSMLParser.instance.RegisterTag(new OpenPageClickableText());
+            BeatSaberMarkupLanguage.BSMLParser.instance.RegisterTag(new OpenPagePageButtonTag());
+            BeatSaberMarkupLanguage.BSMLParser.instance.RegisterTag(new OpenPagePrimaryButtonTag());
 
             zenjector.UseLogger(logger);
             zenjector.Install<MenuInstaller>(Location.Menu);
