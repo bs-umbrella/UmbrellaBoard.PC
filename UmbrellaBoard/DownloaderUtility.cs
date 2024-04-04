c11469decb8ff155679b4d8733ddbffcb80cecbd
Riley Prince <NuggoDEV@gmail.com>
2024-04-04 18:16:43 +1030

    Check if headers are null before adding to request. Swapped if statement around to check for UnityWebRequest.Result.Success instead of multiple fail cases.

--- UmbrellaBoard/DownloaderUtility.cs
+++ UmbrellaBoard/DownloaderUtility.cs
@@ -38,22 +38,28 @@ namespace UmbrellaBoard
             }
 
             UnityWebRequest uwr = new UnityWebRequest(new Uri(url), "GET");
-            foreach (var item in headers)
-                uwr.SetRequestHeader(item.Key, item.Value);
+            
+            if (headers != null)
+            {
+                foreach (var item in headers)
+                    uwr.SetRequestHeader(item.Key, item.Value);
+            }
+            
 
             uwr.SendWebRequest();
 
-            while (!uwr.isDone)
+            while (uwr.result == UnityWebRequest.Result.InProgress)
                 Thread.Sleep(10);
-            if (uwr.result != UnityWebRequest.Result.ProtocolError && uwr.result != UnityWebRequest.Result.ConnectionError)
-            {
-                response.httpCode = 200;
-                response.content = uwr.downloadHandler.data;
-            }
-            else
+
+            if (uwr.result == UnityWebRequest.Result.Success)
             {
                 response.httpCode = 404;
                 response.content = null;
+            }
+            else
+            {
+                response.httpCode = 200;
+                response.content = uwr.downloadHandler.data;
             }
 
             return response;
