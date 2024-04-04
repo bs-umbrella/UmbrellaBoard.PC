113a9793a069d51be721e0ae43cc279c6a4883b9
Riley <90689870+speecil@users.noreply.github.com>
2024-04-04 21:27:00 +1100

    Comment out sprite cache for now

--- UmbrellaBoard/UI/Views/CommunitiesView.cs
+++ UmbrellaBoard/UI/Views/CommunitiesView.cs
@@ -192,15 +192,14 @@ namespace UmbrellaBoard.UI.Views
                 CommunityName = communityName;
                 CommunityURL = communityURL;
                 
-                if (SpriteCache.GetSpriteFromCache(backgroundURL) is Sprite sprite)
-                {
-                    _communityBackground.sprite = sprite;
-                    return this;
-                }
-                
+                //if (SpriteCache.GetSpriteFromCache(backgroundURL) is Sprite sprite)
+                //{
+                //    _communityBackground.sprite = sprite;
+                //    return this;
+                //}
                 _communityBackground.SetImageAsync(backgroundURL);
-                SpriteCache.AddSpriteToCache(backgroundURL, _communityBackground.sprite);
-                SpriteCache.MaintainSpriteCache();
+                //SpriteCache.AddSpriteToCache(backgroundURL, _communityBackground.sprite);
+                //SpriteCache.MaintainSpriteCache();
                 return this;
             }
 
